2c04ab81adeaf1fc306ff6a457b02ac0a51d699d
SvetaKuleshova <morfeya.2011@mail.ru>
2023-08-09 03:39:06 +0300

    Initial commit

---
+++ src/main/java/org/example/FormDate.java
@@ -0,0 +1,6 @@
+package org.example;
+public class FormDate {
+     public int day;
+     public int month;
+     public int year;
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        FormDate birthday = new FormDate();
+        post.birthday = new FormDate();
+        birthday.day = 13;
+        birthday.month = 6;
+        birthday.year = 1999;
+
+
+        post.name = "Иван";
+        post.passport = "4444№44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999) - 999 - 99 - 99";
+        post.surname = "Иванов";
+        post.subscription = true;
+    }
+}
